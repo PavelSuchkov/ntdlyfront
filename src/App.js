ece34d597292c2cabc5ca96cfa17c25e446184b7
PavelSuchkov <suchkovp@gmail.com>
2022-06-23 13:35:46 +0300

    added envConfig to babelrc (important!!!!) added mote components changed: ReactMarkdown version (important!!!)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pages from "./pages";
-import GlobalStyle from "../final/components/GlobalStyle";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import  GlobalStyles  from './components/GlobalStyles';
+import Pages from './pages/index';
+const uri = process.env.REACT_APP_API_URL;
+
+const cache = new InMemoryCache();
+
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true
+});
 
 const App = () => {
   return (
-    <div>
-      <GlobalStyle />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
       <Pages />
-    </div>);
+    </ApolloProvider>);
 };
 ReactDOM.render(<App />, document.getElementById("root"));
